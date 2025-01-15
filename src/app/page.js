815195a7684d45f952b120a463898f1b633def70
katelijacobsen <katjamaehleke98@gmail.com>
2025-01-15 21:02:24 +0100

    being a bit naughty for now

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,25 @@
-"use client"
-import { motion } from 'motion/react';
-import Link from 'next/link';
-import UnicornScene from './components/GradientBackground.tsx';
+"use client";
+import { motion } from "motion/react";
+import Link from "next/link";
+import UnicornScene from "./components/GradientBackground.tsx";
+import { useEffect } from "react";
 
 export default function Home() {
-    return (
-        <main className="relative w-full h-screen">
-            <UnicornScene projectId={"data/4flspySlCrfvkfRH7Co8.json"}/>
-        </main>
-    );
+  useEffect(() => {
+    setTimeout(() => {
+      const anchors = document.getElementsByTagName("a");
+      for (const anchor of anchors) {
+        console.log(anchor.href);
+        if (anchor.href === "https://unicorn.studio/?utm_source=public-url") {
+          anchor.style = "opacity: 0;";
+        }
+      }
+    }, 100);
+  }, []);
+
+  return (
+    <main className="relative w-full h-screen">
+      <UnicornScene projectId={"data/4flspySlCrfvkfRH7Co8.json"} />
+    </main>
+  );
 }
